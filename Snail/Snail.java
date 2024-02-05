5fd4f208755be31df7d5e841d02b94ff7ff5c4b6
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-02-05 14:46:41 +0100

    formatted Snail

--- Snail/Snail.java
+++ Snail/Snail.java
@@ -1,62 +1,64 @@
 import java.util.ArrayList;
 
-public class Snail{
-	public static void main(String[] args){
-		ArrayList<Integer[][]> snailList = new ArrayList<>();
-		snailList.add(new Integer[][]{
-			new Integer[]{1, 2, 3},
-			new Integer[]{4, 5, 6},
-			new Integer[]{7, 8, 9}
-		});
-		snailList.add(new Integer[][]{
-			new Integer[]{1, 2, 3, 2, 2},
-			new Integer[]{4, 5, 6, 3, 3},
-			new Integer[]{7, 8, 9, 4, 4},
-			new Integer[]{7, 8, 9, 5, 5},
-			new Integer[]{7, 8, 9, 6, 6}
-		});
-		
-		for(Integer[][] snail : snailList){
-			Integer[] result = snail(Integer.class, snail);
-			
-			System.out.println("SNAIL: " + snailToString(snail) + " -> " + arrayToString(result));
-		}
-	}
-	
-	private static <T> String arrayToString(T[] arr){
-		String output = "";
-		
-		output += "  { ";
-		
-		for(int i = 0; i < arr.length; i++){
-			if(i != 0){
-				output += ", ";
-			}
-			
-			output += arr[i];
-		}
-		
-		output += " }";
-		
-		return output;
-	}
-	
-	private static <T> String snailToString(T[][] snail){
-		String output = "";
-		
-		output += "{\n";
-		
-		for(T[] arr : snail){
-			output += "  " + arrayToString(arr) + ",\n";
-		}
-			
-		output += "}";
-		
-		return output;
-	}
-	
-	private static <T> T[] snail(Class<T> clazz, T[][] arr2D){
-		TwoDimensionalArrayData<T> data = new TwoDimensionalArrayData<>(clazz, arr2D);
-		return data.transform();
-	}
+public class Snail {
+  public static void main(String[] args) {
+    ArrayList<Integer[][]> snailList = new ArrayList<>();
+    snailList.add(
+        new Integer[][] {
+          new Integer[] {1, 2, 3},
+          new Integer[] {4, 5, 6},
+          new Integer[] {7, 8, 9}
+        });
+    snailList.add(
+        new Integer[][] {
+          new Integer[] {1, 2, 3, 2, 2},
+          new Integer[] {4, 5, 6, 3, 3},
+          new Integer[] {7, 8, 9, 4, 4},
+          new Integer[] {7, 8, 9, 5, 5},
+          new Integer[] {7, 8, 9, 6, 6}
+        });
+
+    for (Integer[][] snail : snailList) {
+      Integer[] result = snail(Integer.class, snail);
+
+      System.out.println("SNAIL: " + snailToString(snail) + " -> " + arrayToString(result));
+    }
+  }
+
+  private static <T> String arrayToString(T[] arr) {
+    String output = "";
+
+    output += "  { ";
+
+    for (int i = 0; i < arr.length; i++) {
+      if (i != 0) {
+        output += ", ";
+      }
+
+      output += arr[i];
+    }
+
+    output += " }";
+
+    return output;
+  }
+
+  private static <T> String snailToString(T[][] snail) {
+    String output = "";
+
+    output += "{\n";
+
+    for (T[] arr : snail) {
+      output += "  " + arrayToString(arr) + ",\n";
+    }
+
+    output += "}";
+
+    return output;
+  }
+
+  private static <T> T[] snail(Class<T> clazz, T[][] arr2D) {
+    TwoDimensionalArrayData<T> data = new TwoDimensionalArrayData<>(clazz, arr2D);
+    return data.transform();
+  }
 }
--- Snail/TwoDimensionalArrayData.java
+++ Snail/TwoDimensionalArrayData.java
@@ -1,100 +1,97 @@
 import java.lang.reflect.Array;
 
-public class TwoDimensionalArrayData<T>{
-	private SnailPoint min;
-	private SnailPoint max;
-	private SnailPoint current;
-	private SnailPoint modifier;
-	
-	private T[][] twoDimensionalArray;
-	
-	private Class<T> clazz;
-	
-	public TwoDimensionalArrayData(Class<T> clazz, T[][] twoDimensionalArray){
-		if(!isValid2DArray(twoDimensionalArray)){
-			throw new IllegalArgumentException("input must be a n*n 2 dimensional array with n >= 1");
-		}
-		
-		this.twoDimensionalArray = twoDimensionalArray;
-		
-		min = new SnailPoint(0,0);
-		max = new SnailPoint(this.twoDimensionalArray.length - 1, this.twoDimensionalArray.length - 1);
-		current = min;
-		modifier = new SnailPoint(1,0);
-		
-		this.clazz = clazz;
-	}
-	
-	public T[] transform(){
-		T[] result = createGenericArray(twoDimensionalArray.length * twoDimensionalArray.length);
-		int index = 0;
-		
-		while(true){
-			result[index] = getValue(current);
-			index++;
-			
-			if(index == result.length){
-				break;
-			}
-			
-			SnailPoint newPoint = current.add(modifier);
-		
-			if(!isInBounds(newPoint)){
-				turn();
-				newPoint = current.add(modifier);
-			}
-			
-			current = newPoint;
-		}
-		
-		return result;
-	}
-	
-	@SuppressWarnings("unchecked")
-	private T[] createGenericArray(int capacity){
-		return (T[]) Array.newInstance(clazz, capacity);
-	}
-	
-	private void turn(){
-		modifier = modifier.rotate();
-		shrinkBounds();
-	}
-	
-	private boolean isInBounds(SnailPoint point){
-		return (
-			point.x() >= min.x() &&
-			point.x() <= max.x() &&
-			point.y() >= min.y() &&
-			point.y() <= max.y()
-		);
-	}
-	
-	private void shrinkBounds(){
-		if(modifier.x() > 0 || modifier.y() > 0){
-			min = min.add(modifier);
-		}
-		else{
-			max = max.add(modifier);
-		}
-	}
-	
-	private T getValue(SnailPoint point){
-		return twoDimensionalArray[point.y()][point.x()];
-	}
-	
-	private boolean isValid2DArray(T[][] twoDimensionalArray){
-		if(twoDimensionalArray == null || twoDimensionalArray.length == 0){
-			return false;
-		}
-		
-		int length = twoDimensionalArray.length;
-		
-		for(T[] arr : twoDimensionalArray){
-			if(arr.length != length){
-				return false;
-			}
-		}
-		
-		return true;
-	}
+public class TwoDimensionalArrayData<T> {
+  private SnailPoint min;
+  private SnailPoint max;
+  private SnailPoint current;
+  private SnailPoint modifier;
+
+  private T[][] twoDimensionalArray;
+
+  private Class<T> clazz;
+
+  public TwoDimensionalArrayData(Class<T> clazz, T[][] twoDimensionalArray) {
+    if (!isValid2DArray(twoDimensionalArray)) {
+      throw new IllegalArgumentException("input must be a n*n 2 dimensional array with n >= 1");
+    }
+
+    this.twoDimensionalArray = twoDimensionalArray;
+
+    min = new SnailPoint(0, 0);
+    max = new SnailPoint(this.twoDimensionalArray.length - 1, this.twoDimensionalArray.length - 1);
+    current = min;
+    modifier = new SnailPoint(1, 0);
+
+    this.clazz = clazz;
+  }
+
+  public T[] transform() {
+    T[] result = createGenericArray(twoDimensionalArray.length * twoDimensionalArray.length);
+    int index = 0;
+
+    while (true) {
+      result[index] = getValue(current);
+      index++;
+
+      if (index == result.length) {
+        break;
+      }
+
+      SnailPoint newPoint = current.add(modifier);
+
+      if (!isInBounds(newPoint)) {
+        turn();
+        newPoint = current.add(modifier);
+      }
+
+      current = newPoint;
+    }
+
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  private T[] createGenericArray(int capacity) {
+    return (T[]) Array.newInstance(clazz, capacity);
+  }
+
+  private void turn() {
+    modifier = modifier.rotate();
+    shrinkBounds();
+  }
+
+  private boolean isInBounds(SnailPoint point) {
+    return (point.x() >= min.x()
+        && point.x() <= max.x()
+        && point.y() >= min.y()
+        && point.y() <= max.y());
+  }
+
+  private void shrinkBounds() {
+    if (modifier.x() > 0 || modifier.y() > 0) {
+      min = min.add(modifier);
+    } else {
+      max = max.add(modifier);
+    }
+  }
+
+  private T getValue(SnailPoint point) {
+    return twoDimensionalArray[point.y()][point.x()];
+  }
+
+  private boolean isValid2DArray(T[][] twoDimensionalArray) {
+    if (twoDimensionalArray == null || twoDimensionalArray.length == 0) {
+      return false;
+    }
+
+    int length = twoDimensionalArray.length;
+
+    for (T[] arr : twoDimensionalArray) {
+      if (arr.length != length) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
